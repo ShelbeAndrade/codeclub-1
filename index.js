ef4696d71a65d8d70b988dbb5e3d1fd653f07d53
Shelbe Andrade <shelbe@cloudflare.com>
2022-11-21 12:56:59 +0000

    Assignment for Week 2

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
+const randomEntries = {
+    0 : {
+        'reportType': 'Technical Report',
+        'link': 'https://docs.google.com/presentation/d/15_2-GTC4Ihbta4TwJFqTVoSngAlyMsKU9lWCxSeVSW0/edit?usp=share_link'
+    },
+    1 : {
+        'reportType': 'Competitive Report',
+        'link': 'https://docs.google.com/presentation/d/18SigMDLMQhh8i0bivjlzhBaJC-b_UgCRD1DafIpBpqk/edit#slide=id.g142bd8b4bfe_0_31'
+    },
+    2 : {
+        'reportType': 'Business Report',
+        'link': 'https://docs.google.com/presentation/d/1xL97Q-evIjbiG_THdx4zV2T3ucLpdzqTIalXTfNMYkU/edit?usp=share_link'
+    },
+    3 : {
+        'reportType': 'Third-Parties impact test',
+        'link': 'https://drive.google.com/file/d/1lwq_LTx4yuDcGlp7ygskeyVjp1QmUErN/view?usp=share_link'
+    }
+}
+
+function randomEntryGenerator(max) {
+    const randomValue = Math.floor(Math.random() * max)
+    return [randomEntries[randomValue]['reportType'], randomEntries[randomValue]['link']]
+}
+
 export default {
     fetch(request) {
 
        if (request.method == "POST") {
-           return new Response('The request was a POST', {
+           return new Response('The request was a POST, please make a GET request', {
                headers: {
                    'content-type': 'text/plain',
                },
@@ -10,11 +34,19 @@ export default {
        }
 
        else if (request.method == 'GET') {
-           return new Response('The request was a GET', {
-               headers: {
-                   'content-type': 'text/plain',
-               },
-           });
+           let entry = randomEntryGenerator(4)
+           let reportType = entry[0]
+           let link = entry[1]
+
+           let html = `<!DOCTYPE html>
+			<body>
+				<p style="text-align:center;"></p>    
+				<p>`+ reportType +` available <a href = `+ link +`>here</a></p>
+			</body>`
+
+           return new Response(html, {
+			headers: {'content-type': 'text/html;charset=UTF-8',},
+		  });
        }
 
        else {
